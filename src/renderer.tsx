6a96c3c36c87617df127102ef84b63a248ed058d
AL EMRAN <emrancu1@gmail.com>
2026-02-14 17:12:37 +0600

    Update renderer.tsx

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,4 +1,5 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
+import { raw } from 'hono/html'
 import { Link, ViteClient } from 'vite-ssr-components/hono'
 
 export const renderer = jsxRenderer(({ children }) => {
@@ -9,13 +10,7 @@ export const renderer = jsxRenderer(({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>ParallelDev</title>
         <link rel="icon" type="image/png" href="https://pub-05940314903b40afadddd07dd49bbeb6.r2.dev/parallel-dev/favicon.png" />
-          <script type="text/javascript">
-              (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "vh7p7juk80");
-          </script>
+        {raw(`<script type="text/javascript">(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "vh7p7juk80");</script>`)}
         <ViteClient />
         <Link href="/src/style.css" rel="stylesheet" />
       </head>
